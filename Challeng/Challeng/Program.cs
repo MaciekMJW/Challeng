d456d9613265a5bccacd7ecb0ae56b6a5e37f7f1
Jarosław Wasilewski <Jaroslaw.Wasilewski@pw.edu.pl>
2023-04-21 12:56:51 +0200

    dzień 4

---
+++ Challeng/Challeng/Program.cs
@@ -0,0 +1,32 @@
+﻿var name = "Ewa";
+var woman = true;
+var age = 19;
+
+if (woman == true)
+{
+
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age >= 18 && age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age >= 30)
+    {
+        Console.WriteLine("Kobieta po 30-stce");
+    }
+    else if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletnia kobieta");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+}
+if (woman == false && age >= 18)
+{
+    Console.WriteLine("Mężczyzna");
+}
